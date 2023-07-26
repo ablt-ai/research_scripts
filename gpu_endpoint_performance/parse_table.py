486f407692c2d2232baa74cab8e7355698644b6e
wwakabobik <i.vereshchagin@gmail.com>
2023-07-26 18:17:10 +0200

    Added service scripts

--- gpu_endpoint_performance/parse_table.py
+++ gpu_endpoint_performance/parse_table.py
@@ -6,7 +6,7 @@ def convert_to_csv(filename):
     with open(filename, 'r') as file:
         content = file.read()
         table = re.findall(r'benchmark:.*?OPS(.*?)\n\n', content, re.DOTALL)[0]
-        lines = table.split('\n')[1:-1]
+        lines = table.split('\n')[2:-1]
         data = [re.split(r'\s{2,}', line) for line in lines]
         df = pd.DataFrame(data)
         df.columns = ['Name (time in ms)', 'Min', 'Max', 'Mean', 'StdDev',
