d14006b5d9cfe19f2b8f5cc439e114acfb3d39aa
wwakabobik <i.vereshchagin@gmail.com>
2023-07-26 18:31:49 +0200

    Added iterations support

--- gpu_endpoint_performance/conftest.py
+++ gpu_endpoint_performance/conftest.py
@@ -3,8 +3,14 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption("--rounds", action="store", default=1, help="Number of repeats")
+    parser.addoption("--iterations", action="store", default=1, help="Number of iterations")
 
 
 @pytest.fixture
 def rounds(request):
     return int(request.config.getoption("--rounds"))
+
+
+@pytest.fixture
+def iterations(request):
+    return int(request.config.getoption("--iterations"))
--- gpu_endpoint_performance/test_gpu_endpoint_performance.py
+++ gpu_endpoint_performance/test_gpu_endpoint_performance.py
@@ -34,33 +34,33 @@ def exec_async(data):
 
 
 @pytest.mark.parametrize("image_aspect", ['4x3', '16x9'])
-def test_image_aspects(image_aspect, benchmark, rounds):
+def test_image_aspects(image_aspect, benchmark, rounds, iterations):
     image_aspect = image_aspect.replace("x", "")
     image_path = f"{image_storage_url}/{image_aspect}/cyberpunk.png"
-    benchmark.pedantic(exec_async, [[image_path]], rounds=rounds)
+    benchmark.pedantic(exec_async, [[image_path]], rounds=rounds, iterations=iterations)
 
 
 @pytest.mark.parametrize("detail", ['1', '2', '3'])
-def test_image_complexity(detail, benchmark, rounds):
+def test_image_complexity(detail, benchmark, rounds, iterations):
     image_path = f"{image_storage_url}/details/details_{detail}.png"
-    benchmark.pedantic(exec_async, [[image_path]], rounds=rounds)
+    benchmark.pedantic(exec_async, [[image_path]], rounds=rounds, iterations=iterations)
 
 
 @pytest.mark.parametrize("image_type", image_formats)
-def test_image_types(image_type, benchmark, rounds):
+def test_image_types(image_type, benchmark, rounds, iterations):
     image_path = f"{image_storage_url}/formats/robot.{image_type}"
-    benchmark.pedantic(exec_async, [[image_path]], rounds=rounds)
+    benchmark.pedantic(exec_async, [[image_path]], rounds=rounds, iterations=iterations)
 
 
 @pytest.mark.parametrize("aspect_ratio,width,height", [(ratio, size['width'],
                                                         size['height']) for ratio, sizes in image_sizes.items() for size
                                                        in sizes])
-def test_image_sizes(aspect_ratio, width, height, benchmark, rounds):
+def test_image_sizes(aspect_ratio, width, height, benchmark, rounds, iterations):
     image_path = f"{image_storage_url}/sizes/toast_{aspect_ratio.replace(':', 'x')}_{width}x{height}.png"
-    benchmark.pedantic(exec_async, [[image_path]], rounds=rounds)
+    benchmark.pedantic(exec_async, [[image_path]], rounds=rounds, iterations=iterations)
 
 
 @pytest.mark.parametrize("num", [1, 2, 3, 4, 5, 10])
-def test_simultaneous_requests(num, benchmark, rounds):
+def test_simultaneous_requests(num, benchmark, rounds, iterations):
     image_paths = [f"{image_storage_url}/{random.choice(['43', '169'])}/{random.choice(image_names)}" for _ in range(num)]
-    benchmark.pedantic(exec_async, [image_paths], rounds=rounds)
+    benchmark.pedantic(exec_async, [image_paths], rounds=rounds, iterations=iterations)
